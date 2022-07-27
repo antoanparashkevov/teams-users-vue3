eba1574fe488fd42d2f84b04186475a799820211
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 11:26:46 +0300

    Register two routes and aware Vue app for router constant

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
-import {createRouter,createWebHistory} from "vue-router";
-
-const router = createRouter({
-    history:createWebHistory(),
-    routes:[]
-})
-
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from "vue-router";
+import TeamsList from "@/components/teams/TeamsList";
+import UsersList from "@/components/users/UsersList";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/teams', component: TeamsList},//our-domain.com/teams TeamsList
+        {path: '/users', component: UsersList}//our-domain.com/users UsersList
+    ]
+})
+
+const app = createApp(App)
+
+//built-in method that allows to connect our Vue app to third party packages.
+app.use(router)
+
+app.mount('#app')
