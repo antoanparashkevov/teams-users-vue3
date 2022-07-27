37d0d08bc4ecca85c974f8ad16f4fba0e7955df7
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 13:14:57 +0300

    Loading a dynamic data using created() lifecycle into TeamMember cmp

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -1,32 +1,48 @@
 <template>
-<section>
-  <h2>{{TeamName}}</h2>
-  <ul>
-    <user-item
-    v-for="member in members"
-    :key="member.id"
-    :name="member.fullName"
-    :role="member.role"
-    ></user-item>
-  </ul>
-</section>
+  <section>
+    <h2>{{teamName}}</h2>
+    <ul>
+      <user-item
+          v-for="member in members"
+          :key="member.id"
+          :name="member.fullName"
+          :role="member.role"
+      ></user-item>
+    </ul>
+  </section>
 </template>
 
 <script>
 import UserItem from "@/components/users/UserItem";
+
 export default {
   name: "TeamsMember",
-  components:{
+  components: {
     UserItem
   },
-  data(){
+  inject: ['users', 'teams'],
+  data() {
     return {
-      TeamName:'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      teamName: '',
+      members: []
     }
+  },
+  // this is one of the lifecycle hooks that will be called when the component is created.
+  //not already shown on the screen, but once all data was available.
+  created() {
+    console.log(this.$route)//object of configurations for a specific route
+    // this.$route.path // /teams/t1 for team1 ...
+    // this.$route.params//object that holds all route parameters
+    const teamId = this.$route.params.teamsId
+    const selectedTeam = this.teams.find(obj => obj.id === teamId);//now this is an object with our Team
+    const members = selectedTeam.members//array of members
+    let selectedMembers = [];
+    for (let member of members) {
+      const currUser = this.users.find(obj => obj.id === member)
+      selectedMembers.push(currUser)
+    }
+    this.teamName = selectedTeam.name
+    this.members = selectedMembers
   }
 }
 </script>
