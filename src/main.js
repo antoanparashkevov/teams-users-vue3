80cc0a660e1bbd2d358a4f24ccd99932ac186953
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 13:43:36 +0300

    Add two more routes. 1: '/', 2: notFound page

--- src/main.js
+++ src/main.js
@@ -3,20 +3,25 @@ import {createRouter, createWebHistory} from "vue-router";
 import TeamsList from "@/components/teams/TeamsList";
 import UsersList from "@/components/users/UsersList";
 import TeamMembers from "@/components/teams/TeamMembers";
+import NotFound from "@/components/NotFound";
 
 import App from './App.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', redirect: '/teams'},
         {path: '/teams', component: TeamsList},//our-domain.com/teams TeamsList
         {path: '/users', component: UsersList},//our-domain.com/users UsersList
 
-        {path:'/teams/:teamsId',component: TeamMembers}//our-domain.com/teams/anything
+        {path: '/teams/:teamsId', component: TeamMembers},//our-domain.com/teams/anything
         //using a dynamic route parameter for individual team
+
+        {path: '/:notFound(.*)', component:NotFound}
+
     ],
     //overwrite the active class for router-link tag
-    linkActiveClass:'active'
+    linkActiveClass: 'active'
 })
 
 const app = createApp(App)
