a301692cefeed5af11b8541ca5971d96f309eb1b
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 10:10:07 +0300

    add TheNavigation component and test it

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-navigation></the-navigation>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheNavigation from "@/components/nav/TheNavigation";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheNavigation
   }
 }
 </script>
