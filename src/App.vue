de53f7fe0fc62d7a02d77e2586f4ddbbf8873a0e
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 10:35:38 +0300

    providing some data to use outside of App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,30 @@ export default {
   },
   data(){
     return {
-      activePage:'teams-list'
+      activePage:'teams-list',
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Svetoslav Ivanov', role: 'Engineer' },
+        { id: 'u3', fullName: 'Nadezhda Dolashka', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Ionov', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marin Petrov', role: 'Consultant' },
+      ],
     }
   },
   methods:{
     setActivePage(page){
       this.activePage = page;
+    }
+  },
+  provide(){
+    return {
+      teams:this.teams,
+      users:this.users
     }
   }
 
