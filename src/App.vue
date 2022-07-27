2781de184c933112b73847f890806127c2149d6f
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 10:21:03 +0300

    Modifying the navigation bar

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import TheNavigation from "@/components/nav/TheNavigation";
+
 export default {
   name: 'App',
   components: {
@@ -13,12 +14,15 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0
+}
+
+html {
+  font-family: sans-serif;
 }
 </style>
--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -1,5 +1,16 @@
 <template>
-<h1>test</h1>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <button>Teams</button>
+        </li>
+        <li>
+          <button>Users</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -9,5 +20,42 @@ export default {
 </script>
 
 <style scoped>
+header {
+  width:100%;
+  height: 5rem;
+  background-color: #11005c;
+}
+
+nav{
+  height: 100%;
+}
+ul{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li{
+  margin: 0 2rem;
+}
+button {
+  font: inherit;
+  background: transparent;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: white;
+  padding:0.5rem 1.5rem;
+  display: inline-block;
+}
+button:hover,
+button:active{
+  color: #f1a80a;
+  border-color: #f1a80a;
+  background-color: #1a037e;
+}
 
 </style>
