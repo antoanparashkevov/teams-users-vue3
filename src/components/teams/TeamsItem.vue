f3116884430041b57f69dd2c2b62888772edb983
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 10:47:35 +0300

    Created,registered TeamsItem cmp and test it

---
+++ src/components/teams/TeamsItem.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Test from TeamsItem</h1>
+</template>
+
+<script>
+export default {
+  name: "TeamsItem"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/teams/TeamsList.vue
+++ src/components/teams/TeamsList.vue
@@ -1,10 +1,15 @@
 <template>
 <h1>Teams List</h1>
+  <teams-item></teams-item>
 </template>
 
 <script>
+import TeamsItem from "@/components/teams/TeamsItem";
 export default {
   name: "TeamsList",
+  components:{
+    TeamsItem
+  }
 }
 </script>
 
