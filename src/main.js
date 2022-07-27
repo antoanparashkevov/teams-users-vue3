17476b91162e68d3b7b6a6f0c5a2dac6b120efa7
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-27 11:19:27 +0300

    install an official router package and register it

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import {createRouter,createWebHistory} from "vue-router";
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes:[]
+})
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
